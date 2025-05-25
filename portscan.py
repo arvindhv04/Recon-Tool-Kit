1ab482a7149dead93f98430d17a6ca339d56578a
arvindh <arvindhvaidya@yahoo.com>
2025-05-25 13:07:24 +0530

    third commit

--- portscan.py
+++ portscan.py
@@ -1,4 +1,5 @@
 import socket
+import argparse
 
 def scan_ports(host):
     common_ports = [21, 22, 23, 25, 53, 80, 110, 143, 443, 8080]
@@ -11,3 +12,24 @@ def scan_ports(host):
         except:
             continue
     return open_ports
+    if __name__ == "__main__":
+
+        def get_service_name(port):
+            try:
+                return socket.getservbyport(port)
+            except:
+                return "Unknown"
+
+        parser = argparse.ArgumentParser(description="Simple port scanner")
+        parser.add_argument("host", help="Target host to scan")
+        args = parser.parse_args()
+
+        print(f"Scanning {args.host}...\n")
+        open_ports = scan_ports(args.host)
+        if open_ports:
+            print("Open ports:")
+            for port in open_ports:
+                service = get_service_name(port)
+                print(f"  Port {port}/tcp ({service}) is open")
+        else:
+            print("No common ports open.")
